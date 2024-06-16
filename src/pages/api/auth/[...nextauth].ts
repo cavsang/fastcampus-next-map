64248f9a303e4255d71db0f4a641c7d7b8d467b4
cavsang <cavsang@naver.com>
2024-06-16 14:09:41 +0900

    0616-3

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export const authOptions= {
         }),
         NaverProvider({
             clientId: process.env.NAVER_CLIENT_ID|| "",
-            clientSecret: process.env.NAVER_CLIENT_SECRET||""
+            clientSecret: process.env.NAVER_CLIENT_SECRET||"yar"
         }),
     ]
 };
