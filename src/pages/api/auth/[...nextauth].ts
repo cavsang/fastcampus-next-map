ae2533dddeeb37fbb09f8801882020575ab7e1a7
cavsang <cavsang@naver.com>
2024-06-16 14:08:51 +0900

    0616-2

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import NaverProvider from "next-auth/providers/naver";
 
 const prisma = new PrismaClient()
 
@@ -12,6 +13,10 @@ export const authOptions= {
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID as string, //undefined 때문에 as string 말고 이렇게 해도되네.
             clientSecret:process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        NaverProvider({
+            clientId: process.env.NAVER_CLIENT_ID|| "",
+            clientSecret: process.env.NAVER_CLIENT_SECRET||""
         }),
     ]
 };
